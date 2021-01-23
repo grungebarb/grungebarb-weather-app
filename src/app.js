daa1f6a8bf002d70c9c594bfbb153378145edcc1
grungebarb <barbarallanso@gmail.com>
2021-01-23 01:06:18 +0100

    Current button, style and icons
    Positioned current button, added touches of style and color and personalized icons (temporary). Last updated date coded in JS

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function formatTime(time) {
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[time.getDay()];
+  let date = time.getDate();
+  let hour = time.getHours();
+  if (hour <10) {
+    hour = `0${hour}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    hour = `0${hour}`;
+  }
+  let fullDate = `${day} ${date}, ${hour}:${minutes}h`
+  return fullDate;
+}
+let currentDate = new Date();
+let currentTime = document.querySelector("#current-time");
+currentTime.innerHTML = formatTime(currentDate);
