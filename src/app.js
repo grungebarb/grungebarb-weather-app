4c472da59ccd002d465ca03a8d5ea9166a2a64b9
grungebarb <barbarallanso@gmail.com>
2021-01-23 22:10:41 +0100

    Improve JS: created search engine with city, country code, temperatur and icon

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Current date
 function formatTime(time) {
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let day = days[time.getDay()];
@@ -16,3 +17,31 @@ function formatTime(time) {
 let currentDate = new Date();
 let currentTime = document.querySelector("#current-time");
 currentTime.innerHTML = formatTime(currentDate);
+
+// Search engine
+function showTemperature(response) {
+  console.log(response);
+  document.querySelector("#current-city").innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  document.querySelector("#current-temperature").innerHTML = `${Math.round(response.data.main.temp)}ºC`;
+  document.querySelector("#humidity").innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
+  document.querySelector("#weather-description").innerHTML = response.data.weather[0].description;
+  // falta darle una vuelta a esta parte, consiguiendo cambiar los iconos por los de openweathermap...
+  let icon = response.data.weather[0].icon;
+  document.querySelector("#current-icon").setAttribute("src", `src/icons/icon_${icon}.png`);
+}
+function search(city) {
+  let apiKey = `b89a2bda363f782379e90e985a8aa5e3`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  search(city);  
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+search("Moralzarzal");
+
+// Icon library
